beb501f98f02d938f68ed75098e037ef0533e6a5
hoak <hoak@glint.markets>
2024-04-09 20:51:55 +0100

    add anchor gen for events

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,41 @@
 #![doc = include_str!("../README.md")]
-
 use anchor_lang::prelude::*;
 use solana_program::pubkey;
 
 pub mod account;
 pub mod constants;
 pub mod errors;
-pub mod id;
 pub mod utils;
+
+#[cfg(feature = "mainnet")]
+declare_id!("ZETAxsqBRek56DhiGXrn75yj2NHU3aYUnxvHXpkf3aD");
+#[cfg(not(feature = "mainnet"))]
+declare_id!("BG3oRikW8d16YjUEmX3ZxHm9SiJzrGtMhsSR8aCw1Cd7");
+
+#[derive(Clone)]
+pub struct ZetaProgram;
+
+impl anchor_lang::Id for ZetaProgram {
+    fn id() -> Pubkey {
+        ID
+    }
+}
+
+#[derive(Clone)]
+pub struct Dex;
+
+impl anchor_lang::Id for Dex {
+    fn id() -> Pubkey {
+        match cfg!(feature = "mainnet") {
+            true => pubkey!("zDEXqXEG7gAyxb1Kg9mK5fPnUdENCGKzWrM21RMdWRq"),
+            false => pubkey!("5CmWtUihvSrJpaUrpJ3H1jUa9DRjYz4v2xs6c3EgQWMf"),
+        }
+    }
+}
+
+anchor_gen::generate_cpi_interface!(idl_path = "idl.json",);
 
 pub use crate::account::*;
 pub use crate::constants::*;
 pub use crate::errors::*;
-pub use crate::id::*;
 pub use crate::utils::*;
